10ae0be06b7758699b96d0dc77c992730ae27d3d
NagendraSutliya <nagendrasutliya@gmail.com>
2024-11-04 17:14:52 +0530

    project - 1 solution

---
+++ projects/1-colorChanger/colorchanger.js
@@ -0,0 +1,40 @@
+const buttons = document.querySelectorAll('.button');
+const body = document.querySelector("body");
+
+buttons.forEach(function(button){
+    console.log(button)
+    button.addEventListener('click', function(event){
+        console.log(event)
+        console.log(event.target)
+        // if (event.target.id === 'grey') {
+        //     body.style.backgroundColor = event.target.id
+        // }
+        // if (event.target.id === 'green'){
+        //     body.style.backgroundColor = event.target.id
+        // }
+        // if(event.target.id === 'blue'){
+        //     body.style.backgroundColor = event.target.id
+        // }
+        // if(event.target.id === 'yellow'){
+        //     body.style.backgroundColor = event.target.id
+        // }
+
+        switch(event.target.id){
+            case 'grey':
+                body.style.backgroundColor = event.target.id;
+                break;
+            case 'green':
+                body.style.backgroundColor = event.target.id;
+                break;
+            case 'blue':
+                body.style.backgroundColor = event.target.id;
+                break;
+            case 'yellow':
+                body.style.backgroundColor = event.target.id;
+                break;
+
+            default:
+                console.log("No color selected")
+        }
+    })
+})
